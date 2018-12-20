78f4f6d550f29d25f907ab41215d793d6e888a3e
Yasir Akbar <yasir.akbar86@hotmail.com>
2018-12-20 14:32:33 +0200

    loader version 1.0

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Loader/Spinner for React Components</h1>
+        <div className="loader"></div>
+      </div>
+    );
+  }
+}
+
+export default App;
