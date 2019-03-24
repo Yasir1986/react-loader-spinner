ec4be73cff1b85925b678882853c7ffd2d0da1e4
Yasir Akbar <yasir.akbar86@hotmail.com>
2019-03-24 17:50:59 +0200

    Fetching api with loader

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React, { Component } from 'react';
 import './App.css';
+import Data from '../src/Data';
+
+
 
 class App extends Component {
   render() {
     return (
       <div>
-        <h1>Loader/Spinner for React Components</h1>
-        <div className="loader"></div>
+          <h1>Main</h1>
+          <Data />
       </div>
     );
   }
---
+++ src/Loader.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class Loader extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Loader/Spinner for React Components</h1>
+        <div className="loader"></div>
+      </div>
+    );
+  }
+}
+
+export default Loader;
